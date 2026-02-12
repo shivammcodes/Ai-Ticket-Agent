a97751c4e2ab614f98fba6190b44c168257ddc76
Shivam Sharma <shivamcodesss@gmail.com>
2026-02-12 08:34:54 +0530

    Validation added using ajv

---
+++ api/utils/validation.js
@@ -0,0 +1,23 @@
+const Ajv =require('ajv');
+const ajv=new Ajv({allErrors: true});
+const schema={
+    type: "object",
+    properties:{
+        summary:{type: "string"},
+        priority:{type: "string", enum:["low","medium","high"]},
+        helpfulNotes:{type: "string"},
+        relatedSkills:{type: "array",items: {type: "string"}}
+    },
+    required:["summary","priority","helpfulNotes","relatedSkills"],
+    additionalProperties: false
+}
+
+const validate=ajv.compile(schema);
+
+exports.ValidateRepsonse =(data)=>{
+const isValid=validate(data);
+if(!isValid){
+    throw error(JSON.stringify(validate.errors));
+}
+ return true
+}
