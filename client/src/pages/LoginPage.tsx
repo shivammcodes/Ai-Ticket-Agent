09b54b6d8d52071d3ca9c9fecb85e877a7197ae0
Shivam Sharma <shivamcodesss@gmail.com>
2026-02-21 13:37:17 +0530

    login page added without the context

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -10,8 +10,40 @@ import {
 } from "@/components/ui/card"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
+  const[email,setEmail]=useState('');
+  const[password,setPassword]=useState('');
+  const navigate=useNavigate();
+  const[loading,setLoading]=useState(false);
+  async function setLogin(e: any){
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const response=await fetch("http://localhost:8080/login",{
+        method: "POST",
+        body:JSON.stringify({email,password}),
+        headers: {"Content-type": "application/json"},
+        credentials: 'include'
+      })
+      const data=await response.json();
+      if(!response.ok){
+        setLoading(false);
+        toast.error(data.error);
+      }
+      else{
+        setLoading(false);
+        toast.success(data.msg);
+        navigate('/')
+      }
+    }
+    catch(error){
+      toast.error("User Login falied");
+    }
+  }
   return (
     <div className="car flex h-screen w-full justify-center items-center">
         <Card className="w-full max-w-sm bg-[#171717]/80 border-border/40">
@@ -33,6 +65,8 @@ const LoginPage = () => {
                 type="email"
                 placeholder="m@example.com"
                 required
+                onChange={(e)=>{setEmail(e.target.value)}}
+                value={email}
                 className="placeholder:text-[#949494] text-muted border-border/40"
               />
             </div>
@@ -40,14 +74,16 @@ const LoginPage = () => {
               <div className="flex items-center">
                 <Label htmlFor="password" className="text-muted">Password</Label>
               </div>
-              <Input id="password" type="password" required className="text-muted border-border/40 placeholder:text-[#949494]" placeholder="****" />
+              <Input id="password" type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} required className="text-muted border-border/40 placeholder:text-[#949494]" placeholder="****" />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full bg-muted text-foreground hover:bg-[#949494]">
-          Login
+        <Button onClick={setLogin} type="submit" disabled={loading} className="w-full bg-muted text-foreground hover:bg-[#949494]">
+          {
+            loading ? "Logging In..." : "Log in"
+          }
         </Button>
       </CardFooter>
     </Card>
