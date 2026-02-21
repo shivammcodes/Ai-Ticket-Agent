3344de9270986d00667ec5bb857aee271675c86f
Shivam Sharma <shivamcodesss@gmail.com>
2026-02-21 15:50:18 +0530

    header updated fully and logout feature added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,50 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
 import Logo from './Logo'
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
+import toast from 'react-hot-toast';
 const Header = () => {
+  const navigate=useNavigate();
+  const context=useContext(UserContext);
+    if(!context){
+      return <h1>UserContext not found</h1>;
+    }
+    const{user,setUserIsAuthenticated,setUser,setLoading}=context;
+    async function logout(){
+      const response=await fetch("http://localhost:8080/logout",{
+        method: "POST",
+        credentials: "include"
+      })
+      const data=await response.json();
+      if(!response.ok){
+        console.log(data.error);
+      }
+      else{
+        toast.success(data.msg);
+        navigate('/login');
+        setUserIsAuthenticated(false);
+        setLoading(false)
+        setUser(null);
+
+      }
+    }
   return (
     <div className='flex justify-between py-5 items-center fixed top-0 left-0 w-full bg-white/1 px-32 backdrop-blur-2xl'>
         <Link to={'/'} className='logo text-2xl text-muted flex items-center gap-x-2'>Assignfy<Logo></Logo></Link>
         <nav className='gap-x-3 flex text-lg items-center'>
-            <Link to={'/login'} className='text-[#949494] text-sm'>Log in</Link>
+            {
+              user==null ? (
+            <>
+              <Link to={'/login'} className='text-[#949494] text-sm'>Log in</Link>
             <Link to={'/signup'}><Button size={'sm'} className='bg-muted text-foreground rounded-2xl hover:bg-[#949494] cursor-pointer'>Signup</Button></Link>
+            </>
+              ):
+              <>
+               <Link to={'/'} className='text-[#949494] text-sm'>Hello, {user.email}</Link>
+               <Link to={'/signup'}><Button size={'sm'} className='bg-muted text-foreground rounded-2xl hover:bg-[#949494] cursor-pointer' onClick={logout}>Logout</Button></Link>
+              </>
+            }
         </nav>
     </div>
   )
